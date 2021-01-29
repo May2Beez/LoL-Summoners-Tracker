62b686ffd83667f0adf572c97bb4c6fa9b5c6118
May2BeeJava <66845996+May2BeeJava@users.noreply.github.com>
2021-01-29 22:00:58 +0100

    Just saving

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import window
 
 
 if __name__ == "__main__":
-    cassiopeia.set_riot_api_key("RGAPI-0e61182d-c5e6-4127-a066-96dc96ac8842")
+    cassiopeia.set_riot_api_key("Key here")
     frame = window.Window()
